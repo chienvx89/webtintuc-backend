b317c281b5e153954b9462311c92d184fd872284
chienvx89evotek <chienvx89.evotek@gmail.com>
2024-04-08 09:00:03 +0700

    init project

---
+++ Website/Controllers/PostController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ILogger<PostController> _logger;
+
+        public PostController(ILogger<PostController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
